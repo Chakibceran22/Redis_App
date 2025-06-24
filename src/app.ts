f6e2444ef2e5d036a52f77a0c6f8da4f6b1a4363
Chakibceran22 <grabachakib008@gmail.com>
2025-06-24 20:35:16 +0100

    adding the data abse config

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.get("/",async (req: Request, res: Response): Promise<void> => {
     res.status(200).send("Hello, World!");
-
 })
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
